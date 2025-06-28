df25ebba17e302d97248b8498c3ecc9e06cd8a39
KotaKambara <78218323+kota200@users.noreply.github.com>
2025-06-28 15:30:33 +0900

    Update API_to_mapping_script.sh

--- API_to_mapping_script.sh
+++ API_to_mapping_script.sh
@@ -1,4 +1,8 @@
-organism_name=${1}
+#$1 is the organism name; $2 is the output prefix; $3 is the reference genome file; $4 is the annotation file in the gff3 format.
+organism_name=${1:?}
+out=${2:?}
+ref=${3:?}
+gff=${4:?}
 
 python API_PM_transcriptome.py ${organism_name}
 
@@ -12,9 +16,22 @@ sed "s/.*&lt;LIBRARY_LAYOUT&gt; &lt;//g" tmp_2 | cut -f1 -d " " | cut -f1 -d "/"
 paste SRR_list_tmp biosample_list_tmp PRJNA_list_tmp sample_name_list_tmp Library_type_list_tmp | sort > tmp_list
 rm *_tmp
 cut -f1 tmp_list | sort | uniq > SRR_list_tmp
-head -n1 PM_TPM_matrix.csv | cut -f2- -d "," | sed "s/;/\n/g" | grep SRR | sort | uniq > SRR_list
 
-if cmp -s ~/PM_DB/SRR_list SRR_list_tmp; then echo "Files are identical."; mv SRR_list_tmp SRR_list; else echo "Files are different"; diff SRR_list SRR_list_tmp > SRR_diff_list;  grep SRR SRR_diff_list | cut -f2 -d " " > SRR_diff_mod; mv SRR_diff_mod SRR_diff_list; for i in `cat SRR_diff_list`; do grep -w ${i} tmp_list >> diff_list; done; ~/PM_DB/mapping_script.sh diff_list; mv SRR_list_tmp SRR_list; fi
+if [ ! -f ${out}_TPM_matrix.csv ]; then
+    touch ${out}_TPM_matrix.csv
+    echo "File ${out}_TPM_matrix.csv doesn't exit. Making a new one..."
+    mapping_script.sh tmp_list ${organism_name} ${ref} ${gff}
+else
+    echo "Updating the file ${out}_TPM_matrix.csv"
+    head -n1 ${out}_TPM_matrix.csv | cut -f2- -d "," | sed "s/;/\n/g" | grep SRR | sort | uniq > SRR_list
+    if cmp -s SRR_list SRR_list_tmp
+      then echo "Files are up to date. Nothing to do."; mv SRR_list_tmp SRR_list
+      else echo "Files are different"; diff SRR_list SRR_list_tmp > SRR_diff_list;  grep SRR SRR_diff_list | cut -f2 -d " " > SRR_diff_mod; mv SRR_diff_mod SRR_diff_list
+        for i in `cat SRR_diff_list`; do grep -w ${i} tmp_list >> diff_list; done
+        mapping_script.sh diff_list; mv SRR_list_tmp SRR_list
+    fi
+fi
+
 
 rm *tmp*
 rm diff_list
