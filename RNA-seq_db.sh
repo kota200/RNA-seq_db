335d20b9dc88f664d717dc7dae78ef93ed976fa9
KotaKambara <78218323+kota200@users.noreply.github.com>
2025-06-28 15:14:26 +0900

    Update RNA-seq_db.sh

--- RNA-seq_db.sh
+++ RNA-seq_db.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 organism_name=${1:?}
-output_prefix=${2:?}
-ref=${3:?}
-gff=${4:?}
+ref=${2:?}
+gff=${3:?}
+output_prefix=${4:?}
 
 echo "Start fetching..."
 ./API_to_mapping_script.sh ${organism_name}
 
 echo "Start mapping..."
-./mapping_script.sh ${output_prefix} ${ref} ${gff}
+./mapping_script.sh  ${ref} ${gff} ${output_prefix}
